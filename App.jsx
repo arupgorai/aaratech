f503c24090edd22a6e94107b660bdca236f8a327
Arup Gorai <aruppgorai@gmail.com>
2025-08-12 20:08:55 +0530

    required packages are added

--- App.jsx
+++ App.jsx
@@ -1,9 +1,16 @@
 import {View, Text} from 'react-native';
+import {Icon} from './src/atoms';
 
 const App = () => {
   return (
     <View>
       <Text>Welcome to React Native!</Text>
+      <Icon
+        type="Entypo"
+        name="attachment"
+        style={{color: '#023595'}}
+        size={16}
+      />
     </View>
   );
 };
